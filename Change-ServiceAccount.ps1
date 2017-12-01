fb13f7fd023099296ca124ee497daa37515c1f5f
Grant Orchard <gorchard@vmware.com>
2017-12-01 20:41:26 +1100

    Add a missing double quote

--- Change-ServiceAccount.ps1
+++ Change-ServiceAccount.ps1
@@ -2,6 +2,6 @@ $var = Get-ChildItem env:
 Write-Output $var
 Write-Output "---------"
 Invoke-Expression "$NTRightsInstallSource +r SeServiceLogonRight -u $serviceAccount"
-$service = Get-WMIObject -ComputerName $hostname -namespace root\cimv2 -class Win32_Service -Filter=Name=$service"
+$service = Get-WMIObject -ComputerName $hostname -namespace "root\cimv2" -class Win32_Service -Filter="Name=$service"
 Write-Output $service
 $service.change($null,$null,$null,$null,$null,$null,$serviceAccount,$servicePassword)
