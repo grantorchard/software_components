fea11ef6dd622231407da1cdf00b7e3d36c6ce9d
Grant Orchard <gorchard@vmware.com>
2017-12-01 20:09:52 +1100

    Initial commit.

---
+++ Change-ServiceAccount.ps1
@@ -0,0 +1,7 @@
+$var = Get-ChildItem env:
+Write-Output $var
+Write-Output "---------"
+Invoke-Expression "$NTRightsInstallSource +r SeServiceLogonRight -u $serviceAccount"
+$service = Get-WMIObject -ComputerName $hostname -namespace root\cimv2 -class Win32_Service -Filter=Name=$service"
+Write-Output $service
+$service.change($null,$null,$null,$null,$null,$null,$serviceAccount,$servicePassword)
